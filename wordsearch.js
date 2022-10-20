e02a43d34d8d98b0c05fd8bb2cd5ef8644b72954
Kristy Gislason <kristygislason@gmail.com>
2022-10-20 13:00:46 -0700

    Pair Programming session

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,46 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+/**
+ *
+ * Word Search
+ * 
+ * Pair programming: A word search solver, as a function that receives a 2D 
+ * array of letters and a word. Check to find the word horizontally and 
+ * vertically. Return true if the word is found, and return false if the word 
+ * is not found
+ *
+ * Authors: @Gozi1, @chchchelsay, @kgislason
+ */
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = verticalJoinner(letters).map(ls => ls.join(''));
+  const backwardsHorizontalJoin = letters.map(ls => ls.reverse().join(''));
+
+  if (searchString(horizontalJoin, word)) return true;
+  if (searchString(backwardsHorizontalJoin, word)) return true;
+  if (searchString(verticalJoin, word)) return true;
+
+  return false;
+};
+
+// Searches through string
+const searchString = (string, word) => {
+  for (let l of string) {
+    if (l.includes(word)) return true;
+  }
+};
+
+// Helper: Flip the array so rows become columns
+const verticalJoinner = (letters) => {
+  const newArray = [];
+  for (let outer in letters[0]) {
+    const tempArray = [];
+    for (let inner in letters) {
+      tempArray.push(letters[inner][outer]);
+    }
+    newArray.push(tempArray);
+  }
+    
+  return newArray;
+};
+
+module.exports = wordSearch;
